f4f68f1b6d8aa16427d9619ebe95e5fd5b2d06dd
Daniel Welsh <danjwelsh24@gmail.com>
2018-03-31 15:38:06 +0100

    :sparkles: Can add and set profiles
    Can now add and save profiles, as well as set them.

--- index.js
+++ index.js
@@ -6,9 +6,12 @@
  *  Manage git profiles
  */
 
-const profiles = require('./profiles.json')
+// const profiles = require('./profiles.json')
 const readline = require('readline')
 const fs = require('fs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const path = require('path')
 
 process.on('unhandledRejection', error => {
   console.error(error)
@@ -21,12 +24,12 @@ const rl = readline.createInterface({
 
 const mode = process.argv[2]
 
-const findProfile = (id) => {
-  profiles.forEach((profile) => {
-    if (profile.id === id) {
-      return profile
+const findProfile = (id, profiles) => {
+  for (let i = 0; i < profiles.length; i++) {
+    if (profiles[i].id === id) {
+      return profiles[i]
     }
-  })
+  }
 }
 
 const readInfo = (question) => {
@@ -37,26 +40,61 @@ const readInfo = (question) => {
   })
 }
 
-const setGitProfile = (profile) => {
-  console.log('setting git profile')
+const setGitProfile = async (profile) => {
+  const commands = [
+    `git config --global user.name "${profile.name}"`,
+    `git config --global user.email "${profile.email}"`,
+    `git config --global user.signingkey ${profile.key}`
+  ]
+
+  const command = commands.join(' && ')
+  await exec(command)
+
+  console.log('Using:', profile.id)
+}
+
+const loadSavedProfiles = () => {
+  return new Promise((resolve, reject) => {
+    fs.stat(path.join(__dirname, '/profiles.json'), (err, stat) => {
+      if (err === null) {
+        resolve(require('./profiles.json'))
+      } else if (err.code === 'ENOENT') {
+        const profiles = []
+        fs.writeFile(path.join(__dirname, '/profiles.json'), JSON.stringify(profiles, null, 2), 'utf8', () => {
+          resolve([])
+        })
+      } else {
+        reject(err)
+      }
+    })
+  })
 }
 
 const main = async () => {
   if (mode === undefined) {
-    console.log('Commands:\n\tset')
+    console.log('Commands:\n\tset\n\tadd')
     process.exit(1)
   }
 
+  // Load set profiles
+  const profiles = await loadSavedProfiles()
+
   switch (mode) {
     case 'set':
-      const profileName = process.argv[3]
-      if (profileName === undefined) {
+      const profileId = process.argv[3]
+      if (profileId === undefined) {
         console.log('Must specify profile name')
         process.exit(1)
       }
 
-      const storedProfile = findProfile(profileName)
-      setGitProfile(storedProfile)
+      const storedProfile = findProfile(profileId, profiles)
+
+      if (!storedProfile) {
+        console.log('Profile could not be found')
+        process.exit(1)
+      }
+
+      await setGitProfile(storedProfile)
       process.exit(0)
 
     case 'add':
@@ -75,11 +113,11 @@ const main = async () => {
       }
 
       profiles.push(profile)
-      fs.writeFile('profiles.json', JSON.stringify(profiles), 'utf8', () => {
-        console.log('Profile saved!')
+      fs.writeFile(path.join(__dirname, '/profiles.json'), JSON.stringify(profiles, null, 2), 'utf8', () => {
+        console.log(`Profile saved as: ${profile.id}`)
+        console.log(`To set your profile, run with the arguments set ${profile.id}`)
       })
 
-      setGitProfile(profile)
       break
     default:
       console.log('Commands:\n\tset\n\tadd')
